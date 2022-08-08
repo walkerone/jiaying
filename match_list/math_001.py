c7cfc45fd19aa1fcfbb0e8085f88c074c8218bc7
walkerone <zzq1401660184@163.co>
2022-08-08 18:57:09 +0800

    d

--- match_list/math_001.py
+++ match_list/math_001.py
@@ -1,43 +1,60 @@
 import random
 
-for i in range( 1 ):
-    m1 = random.randint( 4, 9 )
-    n1 = random.randint( 4, 9 )
+for i in range(1):
+    m1 = random.randint(4, 10)
+    n1 = random.randint(3, random.randint(3, m1))
     q1 = n1 - 1
     sum = m1 * n1 + q1
-    a = "(  )÷{}={}.....{}".format( str( m1 ), str( n1 ), str( q1 ) )
+    a = "(  )÷{}={}.....{}".format(str(m1), str(n1), str(q1))
     # ---------------
-    m2 = random.randint( 4, 9 )
-    n2 = random.randint( 4, 9 )
+    m2 = random.randint(4, 10)
+    n2 = random.randint(3, random.randint(3, m2))
     q2 = n2 - 1
     sum = m2 * n2 + q2
-    b = "{}÷(  )={}.....{}".format( str( sum ), str( n2 ), str( q2 ) )
+    b = "{}÷(  )={}.....{}".format(str(sum), str(n2), str(q2))
     # ---------------
-    m3 = random.randint( 4, 9 )
-    n3 = random.randint( 4, 9 )
-    q3 = n2 - 1
+    m3 = random.randint(4, 10)
+    n3 = random.randint(3, random.randint(3, m3))
+    q3 = n3 - 1
     sum = m3 * n3 + q3
-    c = "{}÷{}=(  ).....{}".format( str( sum ), str( m3 ), str( q3 ) )
+    c = "{}÷{}=(  ).....{}".format(str(sum), str(m3), str(q3))
 
     # ---------------
-    m5 = random.randint( 90, 120 )
-    n5 = random.randint( 90, 120 )
-    q5 = random.randint( 90, 120 )
+    m5 = random.randint(90, 120)
+    n5 = random.randint(90, 120)
+    q5 = random.randint(90, 120)
     sum = m5 + n5 - q5
-    e = "{}+(  )-{}={}".format( str( m5 ), str( n5 ), str( q5 ), str( sum ) )
+    e = "{}+(  )-{}={}".format(str(m5), str(n5), str(q5), str(sum))
 
-    print( a )
-    print( b )
-    print( c )
-    print( e )
+    print(a)
+    print(b)
+    print(c)
+    print(e)
     # ---------------
     # 测试先括号再乘除，再加减
-    m6 = random.randint( 90, 120 )
-    n6 = random.randint( 90, 120 )
-    s6 = random.randint( 5, 9 )
-    h6 = random.randint( s6 - random.randint( 1, 4 ), s6 - 1 )
-    k6 = random.randint( 30, 100 )
+    m6 = random.randint(90, 120)
+    n6 = random.randint(90, 120)
+    s6 = random.randint(5, 9)
+    h6 = random.randint(s6 - random.randint(1, 4), s6 - 1)
+    k6 = random.randint(30, 100)
     sum = m6 + s6 * (s6 - h6) - k6
-    f = "{}+ {}X({}-{})-{}={}".format( str( m6 ), str( s6 ), str( s6 ), str( h6 ), str( k6 ), str( sum ) )
-    print( f )
+    f = "{}+ {}X({}-{})-( )={}".format(str(m6), str(s6), str(s6), str(h6), str(sum))
+    print(f)
     # ---------------
+
+    m7 = random.randint(12, 30)
+    n7 = random.randint(5, 9)
+    sum= m7*n7
+
+    h = "{}÷{}=(  )".format(str(sum),str(n7), )
+    print(h)
+    # ---------------
+
+# with open("unit.py","r+",encoding="utf-8") as file1:
+#     with open("unit.py","w+",encoding="utf-8") as file2:
+#         for line in file1:
+#             if len(line.strip())==0:
+#                 continue
+#             else:
+#                 file2.write(line
+#                             )
